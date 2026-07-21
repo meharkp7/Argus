d6a603cb1d09f94d49744338996240428d518a0a
meharkp7 <meharkapoor7@gmail.com>
2026-07-22 00:25:05 +0530

    fixing errors

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 """ARGUS FastAPI application entrypoint."""
 
 from backend.api.main import app
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
